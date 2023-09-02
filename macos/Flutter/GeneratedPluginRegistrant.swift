dd2933a97ec35dca91a45ac24c004720b022ef12
mr.coder <98886672+mrcoder36@users.noreply.github.com>
2023-09-02 12:52:23 +0530

    Add files via upload

---
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -0,0 +1,14 @@
+//
+//  Generated file. Do not edit.
+//
+
+import FlutterMacOS
+import Foundation
+
+import path_provider_macos
+import text_to_speech
+
+func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
+  TextToSpeechPlugin.register(with: registry.registrar(forPlugin: "TextToSpeechPlugin"))
+}
